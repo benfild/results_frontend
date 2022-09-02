cea887168bc6e9a61c8601703901767b39f02dab
benfild <fildben@gmail.com>
2022-09-02 22:27:29 +0300

    add title service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { RegisterComponent } from "./accounts/register/register.component";
 import { LoginComponent } from "./accounts/login/login.component";
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { PagesComponent } from "./pages/pages.component";
-import { TransferComponent } from "./pages/transfer/transfer.component";
+// import { TransferComponent } from "./pages/transfer/transfer.component";
 import { HomeComponent } from './home/home.component';
-import { BillSummaryComponent } from "./pages/bill-summary/bill-summary.component";
-import { BillComponent } from './pages/bill/bill.component';
+// import { BillSummaryComponent } from "./pages/bill-summary/bill-summary.component";
+// import { BillComponent } from './pages/bill/bill.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, data: { title: 'Home' } },
@@ -21,9 +21,9 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard' } },
-      { path: 'transfers', component: TransferComponent, data: { title: 'Transfers' } },
-      { path: 'utility', component: BillComponent, data: { title: 'Pay Utility' } },
-      { path: 'utility-summary', component: BillSummaryComponent, data: { title: 'Utility Summary' } },
+      // { path: 'transfers', component: TransferComponent, data: { title: 'Transfers' } },
+      // { path: 'utility', component: BillComponent, data: { title: 'Pay Utility' } },
+      // { path: 'utility-summary', component: BillSummaryComponent, data: { title: 'Utility Summary' } },
     ]
   },
 ];
